38ed2b4a0453f040a74977b2c2d86959ab5f84ef
sage <aziz.10.03@mail.ru>
2021-09-28 13:59:46 +0300

    pages/index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,3 +2,37 @@ import '../../style/main.scss'
 import './index.scss'
 import '../../favicons/favicons.js'
 import '../../components/main-menu'
+import datePickerInitial from '../../components/date-picker'
+import dropDownCounterInitial from '../../components/dropdown-counter'
+
+const datePickerDomElements = {
+  parentNode: document.querySelector('.js-date-picker-container'),
+  arrivalInput: document.querySelector('.js-arrival-input'),
+  arrivalSplitBtn: document.querySelector('.js-arrival-input-split-btn'),
+  departureInput: document.querySelector('.js-departure-input'),
+  departureSplitBtn: document.querySelector('.js-departure-input-split-btn'),
+}
+
+datePickerInitial(datePickerDomElements)
+
+const dropDownContainer = document.querySelector('.js-drop-down-counter-container')
+const inputDropDown = document.querySelector('.js-input-drop-down')
+const inputSplitBtn = document.querySelector('.js-input-drop-down-split-btn')
+
+const dropDownOptions = {
+  container: dropDownContainer,
+  input: inputDropDown,
+  inputSplitBtn,
+  countElements: [
+    { name: 'Взрослые', countGroupName: 'guest' },
+    { name: 'Дети', countGroupName: 'guest' },
+    { name: 'Младенцы', countGroupName: 'child' },
+  ],
+  countGroupView: {
+    guest: { counter: 0, views: ['гость', 'гостя', 'гостей'] },
+    child: { counter: 0, views: ['младенец', 'младенца', 'младенцев'] },
+  },
+  placeholder: 'Сколько гостей',
+}
+
+dropDownCounterInitial(dropDownOptions)
