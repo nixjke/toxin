dcad88b13229c1802f639989522e1b7bfb44aa65
sage <aziz.10.03@mail.ru>
2021-09-28 13:48:29 +0300

    pages/rooms

---
+++ src/pages/rooms/rooms.js
@@ -0,0 +1,6 @@
+import '../../style/main.scss'
+import './rooms.scss'
+import '../../components/main-menu'
+import '../../components/hotel-card'
+import '../../components/range-slider'
+import './sections/filter'
