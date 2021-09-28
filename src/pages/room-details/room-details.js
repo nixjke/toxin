d3650d206f0cae653e3e9d51e203d8ae676f9709
sage <aziz.10.03@mail.ru>
2021-09-28 13:50:39 +0300

    pages/room-detalis

---
+++ src/pages/room-details/room-details.js
@@ -0,0 +1,39 @@
+import '../../style/main.scss'
+import './room-details.scss'
+import '../../components/main-menu'
+import '../../components/total-card'
+import '../../components/pie-chart'
+import datePickerInitial from '../../components/date-picker'
+import dropDownCounterInitial from '../../components/dropdown-counter'
+
+const datePickerDomElements = {
+  parentNode: document.querySelector('.js-date-picker-container'),
+  arrivalInput: document.querySelector('.js-arrival-input'),
+  arrivalSplitBtn: document.querySelector('.js-arrival-input-split-btn'),
+  departureInput: document.querySelector('.js-departure-input'),
+  departureSplitBtn: document.querySelector('.js-departure-input-split-btn'),
+}
+
+datePickerInitial(datePickerDomElements)
+
+const dropDownContainer = document.querySelector('.js-drop-down-counter-container')
+const inputDropDown = document.querySelector('.js-input-drop-down')
+const inputSplitBtn = document.querySelector('.js-input-drop-down-split-btn')
+
+const dropDownOptions = {
+  container: dropDownContainer,
+  input: inputDropDown,
+  inputSplitBtn,
+  countElements: [
+    { name: 'Взрослые', countGroupName: 'guest' },
+    { name: 'Дети', countGroupName: 'guest' },
+    { name: 'Младенцы', countGroupName: 'child' },
+  ],
+  countGroupView: {
+    guest: { counter: 0, views: ['гость', 'гостя', 'гостей'] },
+    child: { counter: 0, views: ['младенец', 'младенца', 'младенцев'] },
+  },
+  placeholder: 'Cколько гостей',
+}
+
+dropDownCounterInitial(dropDownOptions)
--- src/pages/rooms/sections/filter/index.js
+++ src/pages/rooms/sections/filter/index.js
@@ -1,65 +1,59 @@
-import datePickerInitial from '../../../../components/date-picker';
-import dropDownCounterInitial from '../../../../components/dropdown-counter';
+import datePickerInitial from '../../../../components/date-picker'
+import dropDownCounterInitial from '../../../../components/dropdown-counter'
 
 const datePickerDomElements = {
   parentNode: document.querySelector('.js-date-picker-container'),
   datePickerInput: document.querySelector('.js-drop-down-input'),
   datePickerSplitBtn: document.querySelector('.js-drop-down-input-split-btn'),
   isCellLower: true,
-};
+}
 
-datePickerInitial(datePickerDomElements);
+datePickerInitial(datePickerDomElements)
 
-const dropDownGuestContainer = document.querySelector(
-  '.js-drop-down-counter-container-guest'
-);
-const inputDropDownGuest = document.querySelector('.js-input-drop-down-guest');
-const inputSplitBtnGuest = document.querySelector(
-  '.js-input-drop-down-guest-split-btn'
-);
+const dropDownGuestContainer = document.querySelector('.js-drop-down-counter-container-guest')
+const inputDropDownGuest = document.querySelector('.js-input-drop-down-guest')
+const inputSplitBtnGuest = document.querySelector('.js-input-drop-down-guest-split-btn')
 
 const dropDownGuestOptions = {
   container: dropDownGuestContainer,
   input: inputDropDownGuest,
   inputSplitBtn: inputSplitBtnGuest,
   countElements: [
-    {name: 'Взрослые', countGroupName: 'guest', startValue: 3},
-    {name: 'Дети', countGroupName: 'guest', startValue: 0},
-    {name: 'Младенцы', countGroupName: 'child', startValue: 1},
+    { name: 'Взрослые', countGroupName: 'guest', startValue: 3 },
+    { name: 'Дети', countGroupName: 'guest', startValue: 0 },
+    { name: 'Младенцы', countGroupName: 'child', startValue: 1 },
   ],
   countGroupView: {
-    guest: {counter: 3, views: ['гость', 'гостя', 'гостей']},
-    child: {counter: 1, views: ['младенец', 'младенца', 'младенцев']},
+    guest: { counter: 3, views: ['гость', 'гостя', 'гостей'] },
+    child: { counter: 1, views: ['младенец', 'младенца', 'младенцев'] },
   },
   placeholder: 'Cколько гостей',
-};
+}
 
-dropDownCounterInitial(dropDownGuestOptions);
+dropDownCounterInitial(dropDownGuestOptions)
 
-const dropDownContainer = document.querySelector(
-  '.js-drop-down-counter-container'
-);
-const inputDropDown = document.querySelector('.js-input-drop-down');
-const inputSplitBtn = document.querySelector('.js-input-drop-down-split-btn');
+const dropDownContainer = document.querySelector('.js-drop-down-counter-container')
+const inputDropDown = document.querySelector('.js-input-drop-down')
+const inputSplitBtn = document.querySelector('.js-input-drop-down-split-btn')
 
 const dropDownOptions = {
   container: dropDownContainer,
   input: inputDropDown,
   inputSplitBtn,
   countElements: [
-    {name: 'Спальни', countGroupName: 'bedrooms', startValue: 2},
-    {name: 'Кровати', countGroupName: 'bed', startValue: 2},
-    {name: 'Ванные комнаты', countGroupName: 'bath', startValue: 0},
+    { name: 'Спальни', countGroupName: 'bedrooms', startValue: 2 },
+    { name: 'Кровати', countGroupName: 'bed', startValue: 2 },
+    { name: 'Ванные комнаты', countGroupName: 'bath', startValue: 0 },
   ],
   countGroupView: {
-    bedrooms: {counter: 2, views: ['спальня', 'спальни', 'спален']},
-    bed: {counter: 2, views: ['кровать', 'кровати', 'кроватей']},
+    bedrooms: { counter: 2, views: ['спальня', 'спальни', 'спален'] },
+    bed: { counter: 2, views: ['кровать', 'кровати', 'кроватей'] },
     bath: {
       counter: 0,
       views: ['ванная комната', 'ванные комнаты', 'ванных комнат'],
     },
   },
   placeholder: 'Удобства номера',
-};
+}
 
-dropDownCounterInitial(dropDownOptions);
+dropDownCounterInitial(dropDownOptions)
