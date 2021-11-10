9be0cefdb20b3a720b54f7a16a35e04d29b9352e
SosiskaPish <aziz.10.03@mail.ru>
2021-11-10 13:56:08 +0300

    fix subMenu

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -1,19 +1,20 @@
+import '../../style/main.scss'
 import './test.scss'
-import '../../libs/Material-icons/index'
-import '../../../node_modules/air-datepicker/air-datepicker.css'
-import '../../components/date-picker/date-picker.scss'
-import AirDatepicker from '../../libs/Air-datepicker/index'
+// import '../../libs/Material-icons/index'
+// import '../../../node_modules/air-datepicker/air-datepicker.css'
+// import '../../components/date-picker/date-picker.scss'
+// import AirDatepicker from '../../libs/Air-datepicker/index'
 
-let Datepicker = new AirDatepicker('.block', {
-  range: true,
-  buttons: ['clear', 'today'],
-  locale: {
-    today: 'Применить',
-  },
-  navTitles: {
-    days: 'MMMM yyyy',
-  },
-  prevHtml: '<span class="material-icons-outlined">arrow_back</span>',
-  nextHtml: '<span class="material-icons-outlined">arrow_forward</span>',
-  startDate: new Date(),
-})
+// let Datepicker = new AirDatepicker('.block', {
+//   range: true,
+//   buttons: ['clear', 'today'],
+//   locale: {
+//     today: 'Применить',
+//   },
+//   navTitles: {
+//     days: 'MMMM yyyy',
+//   },
+//   prevHtml: '<span class="material-icons-outlined">arrow_back</span>',
+//   nextHtml: '<span class="material-icons-outlined">arrow_forward</span>',
+//   startDate: new Date(),
+// })
