95873e301bf30cb382d247277c61cd23a871e2ec
SosiskaPish <aziz.10.03@mail.ru>
2021-11-18 13:08:14 +0300

    fix sub-menu-active

--- src/components/main-menu/MainMenu.js
+++ src/components/main-menu/MainMenu.js
@@ -5,6 +5,7 @@ class MainMenu {
 
   init() {
     this.subMenuAddListener()
+    this._setSubMenuListener()
   }
 
   subMenuAddListener() {
@@ -13,6 +14,21 @@ class MainMenu {
     burgerIcon.addEventListener('click', () => {
       subMenu.classList.toggle('sub-menu-active')
     })
+  }
+
+  _setSubMenuListener() {
+    const subMenuItems = this.parentDom.querySelectorAll('.js-main-menu-item-sub-menu')
+    subMenuItems.forEach(this._addSubMenuItemListener)
+  }
+
+  _addSubMenuItemListener = subMenuItem => {
+    const subMenuLink = subMenuItem.firstChild
+    const subMenu = subMenuLink.nextSibling
+    subMenuItem.addEventListener('click', () => this._toggleSubMenuClass(subMenu))
+  }
+
+  _toggleSubMenuClass(subMenu) {
+    subMenu.classList.toggle('main-menu__sub-menu_opened')
   }
 }
 
